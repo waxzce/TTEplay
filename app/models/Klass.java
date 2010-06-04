872314809c81373088e46baa87ca6e160601c99c
Quentin ADAM <waxzce@kan-op.com>
2010-06-04 12:17:16 +0200

    séparation de Classe et de Promo

---
+++ app/models/Klass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import play.data.validation.*;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author waxzce
+ */
+@Entity
+public class Klass extends Model {
+
+    @Required
+    public String name;
+    @ManyToOne
+    @Required
+    public Promo promo;
+    @ManyToOne
+    @Required
+    public Campus campus;
+}
