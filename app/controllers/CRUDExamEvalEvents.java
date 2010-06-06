a3dbcd9933462af22c36e27e096d3797ec1a8273
Quentin ADAM <waxzce@kan-op.com>
2010-06-06 15:52:17 +0200

    add some crud files

---
+++ app/controllers/CRUDExamEvalEvents.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import models.event.eval.*;
+
+/**
+ *
+ * @author waxzce
+ */
+@CRUD.For(ExamEvalEvent.class)
+public class CRUDExamEvalEvents extends CRUD {
+}
