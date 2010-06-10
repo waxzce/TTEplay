d14849b306599ec7e20cd4f5b81bdf4430b12f24
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 16:05:20 +0200

    tentatice de résoudre les pbs de dates

--- public/javascripts/anytime_start.js
+++ public/javascripts/anytime_start.js
@@ -1,9 +1,19 @@
 
 $(document).ready(function(){
-            $("#object_start").AnyTime_picker(
-            { format: "%Y-%m-%d %H:%i:%s %E %#",
-            formatUtcOffset: "%: (%@)",
-            hideInput: false,
-            placement: "inline" } );
-            });
+    $("#object_start").AnyTime_picker(
+    {
+        format: "%Y-%m-%d %H:%i:%s %E %#",
+        formatUtcOffset: "%: (%@)",
+        hideInput: true,
+        placement: "inline"
+    } );
+});
 
+$(document).ready(function(){
+    $('form').submit(function() {
+        var defaultConv = new AnyTime.Converter();
+        var date = defaultConv.parse($("#object_start").val());
+        var date2 = new Date(date);
+        alert(date2);
+    });
+});
