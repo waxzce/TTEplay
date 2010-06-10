d494e5babf478df4fcc62bef45100add34a399c3
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 14:53:16 +0200

    work in preogress

--- app/controllers/Account.java
+++ app/controllers/Account.java
@@ -5,6 +5,9 @@ import models.event.*;
 import models.person.Person;
 import play.mvc.*;
 import com.google.gson.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import util.FullCalendarEvent;
 
 @With(Secure.class)
 public class Account extends Controller {
@@ -35,13 +38,56 @@ public class Account extends Controller {
     public static void mycalendar() {
         Person user = Person.get(Security.connected());
         List<Event> calendar = user.getCalendar();
-        
-/*        GsonBuilder gsonB = new GsonBuilder();
+        List<FullCalendarEvent> lfes = new ArrayList<FullCalendarEvent>();
+        for (Iterator<Event> it = calendar.iterator(); it.hasNext();) {
+            Event event = it.next();
+            FullCalendarEvent fce = new FullCalendarEvent();
+            fce.allDay = false;
+            fce.className = event.getClass().getName();
+            fce.editable = false;
+            fce.end = event.end;
+            fce.start = event.start;
+            fce.id = event.id;
+            fce.title = event.name;
+            fce.url = "";
+            lfes.add(fce);
+        }
+
+        Gson gson = new Gson();
+
+        String jsoncal = gson.toJson(lfes);
+
+
+        /*        GsonBuilder gsonB = new GsonBuilder();
         gsonB.registerTypeAdapter(Event.class, new EventSerializer());
         
         Gson gson = gsonB.setPrettyPrinting().create();
         String jsonCalendar = gson.toJson(calendar);*/
-        
-        render(calendar, user);
+
+        render(calendar, user, jsoncal);
+    }
+
+    public static void jsonmycal(){
+        Person user = Person.get(Security.connected());
+        List<Event> calendar = user.getCalendar();
+        List<FullCalendarEvent> lfes = new ArrayList<FullCalendarEvent>();
+        for (Iterator<Event> it = calendar.iterator(); it.hasNext();) {
+            Event event = it.next();
+            FullCalendarEvent fce = new FullCalendarEvent();
+            fce.allDay = false;
+            fce.className = event.getClass().getName();
+            fce.editable = false;
+            fce.end = event.end;
+            fce.start = event.start;
+            fce.id = event.id;
+            fce.title = event.name;
+            fce.url = "";
+            lfes.add(fce);
+        }
+
+        Gson gson = new Gson();
+
+        String jsoncal = gson.toJson(lfes);
+        renderJSON(jsoncal);
     }
 }
--- app/controllers/CRUDStudends.java
+++ app/controllers/CRUDStudends.java
@@ -8,7 +8,11 @@ import models.person.Student;
 import play.mvc.*;
 import play.*;
 import controllers.CRUD;
+import java.util.List;
 import models.*;
+import models.person.Staff;
+import play.db.jpa.JPASupport;
+import play.exceptions.TemplateNotFoundException;
 
 /**
  *
@@ -18,4 +22,29 @@ import models.*;
 @Check("staff")
 @With(Secure.class)
 public class CRUDStudends extends CRUD {
+
+    public static void list(int page, String search, String searchFields, String orderBy, String order) {
+
+        ObjectType type = ObjectType.get(getControllerClass());
+        notFoundIfNull(type);
+        if (page < 1) {
+            page = 1;
+        }
+        String where = (String) request.args.get("where");
+        
+        if (Security.check("localstaff")) {
+            where = (where != null ? where + " and " : "");
+            where = where + "klass.campus.id=" + ((Staff) Security.get()).campus.id.toString();
+        }
+        List<JPASupport> objects = type.findPage(page, search, searchFields, orderBy, order, where);
+        Long count = type.count(search, searchFields, where);
+        Long totalCount = type.count(null, null, where);
+        try {
+            render(type, objects, count, totalCount, page, orderBy, order);
+        } catch (TemplateNotFoundException e) {
+            render("CRUD/list.html", type, objects, count, totalCount, page, orderBy, order);
+        }
+
+
+    }
 }
