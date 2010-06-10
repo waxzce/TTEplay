f8c2c731513c753975831b2b50624bbba0a0dc58
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 18:10:21 +0200

    some from improvement

--- public/javascripts/anytime_end.js
+++ public/javascripts/anytime_end.js
@@ -1,5 +1,12 @@
-
 $(document).ready(function(){
+    var dd = new Date();
+    dd.setTime(parseInt($("#object_end").val()));
+    var dateconv = new AnyTime.Converter({
+        utcFormatOffsetImposed: 0,
+        format:"%Y-%m-%d %H:%i:%s %E %#"
+    });
+
+    $("#object_end").val(dateconv.format(dd));
     $("#object_end").AnyTime_picker(
     {
         format: "%Y-%m-%d %H:%i:%s %E %#",
@@ -7,9 +14,8 @@ $(document).ready(function(){
         hideInput: true,
         placement: "inline"
     } );
-});
-$(document).ready(function(){
     $("input[type='submit']").click(function() {
+
         var defaultConv = new AnyTime.Converter();
         var date = defaultConv.parse($("#object_end").val());
         $("#object_end").val(date.getTime());
--- public/javascripts/anytime_start.js
+++ public/javascripts/anytime_start.js
@@ -1,5 +1,12 @@
-
 $(document).ready(function(){
+    var dd = new Date();
+    dd.setTime(parseInt($("#object_start").val()));
+    var dateconv = new AnyTime.Converter({
+        utcFormatOffsetImposed: 0,
+        format:"%Y-%m-%d %H:%i:%s %E %#"
+    });
+
+    $("#object_start").val(dateconv.format(dd));
     $("#object_start").AnyTime_picker(
     {
         format: "%Y-%m-%d %H:%i:%s %E %#",
@@ -7,10 +14,8 @@ $(document).ready(function(){
         hideInput: true,
         placement: "inline"
     } );
-});
-
-$(document).ready(function(){
     $("input[type='submit']").click(function() {
+       
         var defaultConv = new AnyTime.Converter();
         var date = defaultConv.parse($("#object_start").val());
         $("#object_start").val(date.getTime());
