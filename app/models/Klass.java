444c3d76a877a5a270691bd2517546c77e945b8b
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 19:23:06 +0200

    add practical exam support

--- app/models/Klass.java
+++ app/models/Klass.java
@@ -5,9 +5,12 @@
 package models;
 
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.ManyToOne;
 import play.data.validation.*;
+import play.db.jpa.JPA;
 import play.db.jpa.Model;
 
 /**
@@ -25,4 +28,12 @@ public class Klass extends Model {
     @ManyToOne
     @Required
     public Campus campus;
+
+
+    public static List<Klass> findForCampus(Campus campus) {
+        EntityManager em = JPA.em();
+        //  List l = em.createQuery("from CampusEvent ce, ExamEvent ee, GlobalEvent ge, LessonEvent le, PersoEvent pe where ce.campus = :campus").setParameter("campus", s.promo.campus).getResultList();
+        List<Klass> l = em.createQuery("from Klass k where campus = :campus").setParameter("campus", campus).getResultList();
+        return l;
+    }
 }
