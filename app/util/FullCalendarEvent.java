ad98c4503454bf709cd0a9f054fd8252325a9c44
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 14:53:25 +0200

    wip

---
+++ app/util/FullCalendarEvent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author waxzce
+ */
+public class FullCalendarEvent {
+
+    public Long id;
+    public String title;
+    public Boolean allDay;
+    public Long start;
+    public Long end;
+    public String url;
+    public String className;
+    public Boolean editable;
+}
