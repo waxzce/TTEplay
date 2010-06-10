fafd4795fbfcbe77a4c7e25cff2144ee44a85563
Quentin ADAM <waxzce@kan-op.com>
2010-06-10 22:29:09 +0200

    some improvement

--- app/controllers/CRUDPracticalEvalEvents.java
+++ app/controllers/CRUDPracticalEvalEvents.java
@@ -6,6 +6,13 @@ package controllers;
 
 import models.event.eval.*;
 import play.mvc.With;
+import models.event.eval.*;
+import play.mvc.*;
+import controllers.CRUD;
+import java.util.List;
+import models.person.Staff;
+import play.db.jpa.JPASupport;
+import play.exceptions.TemplateNotFoundException;
 
 /**
  *
@@ -15,4 +22,29 @@ import play.mvc.With;
 @Check("staff")
 @With(Secure.class)
 public class CRUDPracticalEvalEvents extends CRUD {
+
+     public static void list(int page, String search, String searchFields, String orderBy, String order) {
+
+        ObjectType type = ObjectType.get(getControllerClass());
+        notFoundIfNull(type);
+        if (page < 1) {
+            page = 1;
+        }
+        String where = (String) request.args.get("where");
+
+        if (Security.check("localstaff")) {
+            where = (where != null ? where + " and " : "");
+            where = where + "klass.campus.id=" + ((Staff) Security.get()).campus.id.toString();
+        }
+        List<JPASupport> objects = type.findPage(page, search, searchFields, orderBy, order, where);
+        Long count = type.count(search, searchFields, where);
+        Long totalCount = type.count(null, null, where);
+        try {
+            render(type, objects, count, totalCount, page, orderBy, order);
+        } catch (TemplateNotFoundException e) {
+            render("CRUD/list.html", type, objects, count, totalCount, page, orderBy, order);
+        }
+
+
+    }
 }
